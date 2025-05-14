d6d44a2dbec561c33cc353a1c0f0ff7f77c3852a
AleSantoni <alesantoni31@gmail.com>
2025-05-14 20:13:46 -0300

    migracion a base postgrade

--- FichaMedica/FichaMedica/settings.py
+++ FichaMedica/FichaMedica/settings.py
@@ -134,13 +134,22 @@ WSGI_APPLICATION = 'FichaMedica.wsgi.application'
  """
 
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'checkeate_db',         
+        'USER': 'postgres',
+        'PASSWORD': 'ale123',     
+        'HOST': 'localhost',
+        'PORT': '5433',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
